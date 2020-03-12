75d2a6b26d2659d07329356919db25d3c0b40f93
Shamira Naznin <shaminzn@gmail.com>
2020-03-12 11:14:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
 function forLoop(argument) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
+      console.log(`"I am 1 strange loop."`)
+    } else {
       console.log(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
     }
 
