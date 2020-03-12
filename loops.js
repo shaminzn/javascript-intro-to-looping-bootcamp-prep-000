7a7f01f9bb1f531674fc4fc71ea3d8427d2f3e17
Shamira Naznin <shaminzn@gmail.com>
2020-03-12 11:11:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(argument) {
   for (let i = 0; i < 25; i++) {
-    console.log(`I am ${i} strange loop ${i === 0 ? '': 's'}.`)
+    console.log(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
   }
 }
